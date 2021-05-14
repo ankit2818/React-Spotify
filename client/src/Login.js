77fa338a695cd6f4d30120dfe8fd5a658c95e08d
Ankit H. Vishwakarma <ankitvi.786.av@gmail.com>
2021-05-14 16:46:56 +0530

    Add files via upload

---
+++ client/src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+
+const AUTH_URL = process.env.REACT_APP_AUTH_URL
+
+
+export default function Login() {
+    return (
+        <Container className="d-flex justify-content-center align-items-center" style={{minHeight: "100vh"}}>
+            <a className="btn btn-success btn-lg" href={AUTH_URL}>Login With Spotify</a>
+        </Container>
+    )
+}
