41f52a4c18a33c450a7d3424a99bfe4aac949754
Ankit H. Vishwakarma <ankitvi.786.av@gmail.com>
2021-05-14 16:48:07 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const lyricsFinder = require("lyrics-finder")
+const spotifyWebApi = require('spotify-web-api-node')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    });
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch(() => res.sendStatus(400))
+})
+
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+    console.log(refreshToken)
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken
+    });
+
+    spotifyApi.refreshAccessToken().then((data) => {
+        res.json({
+            accessToken: data.body.accessToken,
+            expiresIn: data.body.expiresIn
+        })
+        
+    }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
+    })
+    
+})
+
+app.get("/lyrics", async (req, res) => {
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track) || "No Lyrics Found"
+    res.json({lyrics})
+})
+
+
+app.listen(5000)
